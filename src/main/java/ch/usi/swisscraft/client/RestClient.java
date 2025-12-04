682450c42295475b5045feff3b042106fd4fb588
Manuel Montaruli <manuelmontaruli@gmail.com>
2025-12-04 09:32:07 +0100

    Swisstopo connected

--- src/main/java/ch/usi/swisscraft/client/RestClient.java
+++ src/main/java/ch/usi/swisscraft/client/RestClient.java
@@ -1,17 +1,36 @@
 package ch.usi.swisscraft.client;
 
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
 public class RestClient {
     public static void main(String[] args) {
         HttpClient client = HttpClient.newHttpClient();
+        int xmin = 2600000;
+        int ymin = 1200000;
+        int increment = 16;
+        int xmax = xmin + increment - 1;
+
+        String geomJson = String.format(
+            "{\"type\":\"LineString\",\"coordinates\":[[%d,%d],[%d,%d]]}",
+            xmin, ymin, xmax, ymin
+        );
+
+        String url = String.format(
+            "https://api3.geo.admin.ch/rest/services/profile.json?geom=%s&sr=%d&nb_points=%d",
+            URLEncoder.encode(geomJson, StandardCharsets.UTF_8),
+            2056,
+            increment
+        );
+
+
 
         HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("https://reqres.in/api/users?page=2"))
-            .header("x-api-key", "reqres-free-v1")
+            .uri(URI.create(url))
             .GET()
             .build();
 
